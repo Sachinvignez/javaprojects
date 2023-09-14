cb681f89efbf30a4970d964209af987cdc41a8aa
unknown <sachinvignesh80@gmail.com>
2023-09-14 07:55:25 -0700

    Sachin Rockzzz

---
+++ JavaApplication6.java
@@ -0,0 +1,25 @@
+package javaapplication6;
+
+public class javaapplication6 {
+
+    public Static void  main(String[] args) {
+        int a=10;
+        ++a;
+        system.out.println(a);
+        --a;
+        System.out.println(a);
+        //post
+        int k=30;
+        k++;System.out.println(k);
+        k--;
+        System.out.println(k);
+        int s=40
+        s++,s--,--s,++s;
+        System.out.println(++s+s---s);
+        int p1=50,p2=45;
+        System.out.println(p1==p2);
+        System.out.println(p1!=p2);
+        System.out.println("ans:"+(p1==p2));
+        System.out.println("ans":)\n"+(p1!=p2);
+    }
+}
